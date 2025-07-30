0577a14501ac7762f738ae2b9b523879eb242bba
enesyildirim <oyildirim.enes@gmail.com>
2025-07-30 21:04:16 +0300

    first commit

--- app/src/main/java/com/enons/smarthome/MainActivity.kt
+++ app/src/main/java/com/enons/smarthome/MainActivity.kt
@@ -38,10 +38,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartHomeTheme {
-        Greeting("Android")
-    }
-}
